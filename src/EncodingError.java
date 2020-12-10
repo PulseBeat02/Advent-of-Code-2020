ad6b0edc90105f2f652d304aa48a8d4946f341c4
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2020-12-10 18:01:26 -0500

    Changed Encoding Error Extra Loops

--- src/EncodingError.java
+++ src/EncodingError.java
@@ -1,33 +1,31 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class EncodingError {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("encodingerror.txt"));
         List<Long> list = new ArrayList<>();
+        List<Long> prefixSum = new ArrayList<>();
         String line = br.readLine();
+        long sum = 0;
         while (line != null) {
-            list.add(Long.parseLong(line));
+            long l = Long.parseLong(line);
+            prefixSum.add(sum);
+            list.add(l);
+            sum += l;
             line = br.readLine();
         }
         br.close();
         System.out.println("Part One: " + calculatePartOne(list, 25));
-        System.out.println("Part Two: " + getContiguousSet(list, calculatePartOne(list, 25)));
+        System.out.println("Part Two: " + getContiguousSet(list, prefixSum, calculatePartOne(list, 25)));
     }
 
-    public static long getContiguousSet(List<Long> list, long target) {
-        List<Long> prefixSum = new ArrayList<>();
-        long sum = 0;
-        for (long l : list) {
-            prefixSum.add(sum);
-            sum += l;
-        }
+    public static long getContiguousSet(List<Long> list, List<Long> prefixSum, long target) {
+        Set<Long> values = new HashSet<>();
+
         for (int i = 0; i < prefixSum.size(); i++) {
             for (int j = 1; j < prefixSum.size(); j++) {
                 long begin = prefixSum.get(i);
@@ -60,9 +58,11 @@ public class EncodingError {
 
     // Uses Hashing for an O(N) Algorithm
     public static boolean checkValidity(List<Long> list, int index, int preamble) {
-        HashSet<Long> values = new HashSet<>();
+        Set<Long> values = new HashSet<>();
         for (int i = index - preamble; i < index; i++) {
-            if (values.contains(list.get(index) - list.get(i))) return true;
+            if (values.contains(list.get(index) - list.get(i))) {
+                return true;
+            }
             values.add(list.get(i));
         }
         return false;
